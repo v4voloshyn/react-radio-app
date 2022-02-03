36d1e4076db8cc7dd666ad4bf7000656526c4656
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-02-03 19:47:43 +0200

    Made Radio component without syles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Radio } from './Radio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Радіо плеєр українських радіо станцій</h1>
+      <h4>Виберіть жанр, виберіть станцію, насолоджуйтесь музикою</h4>
+      <Radio/>
     </div>
   );
 }
 
 export default App;
+//http://all.api.radio-browser.info/json/stations/bycountry/Ukraine
---
+++ src/Radio.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+
+import { RadioBrowserApi } from "radio-browser-api";
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
+import defaultImg from './assets/img/radio.svg';
+
+export const Radio = () => {
+
+   const [stations, setStations] = useState();
+   const [stationFilter, setStationFilter] = useState('');
+   
+   useEffect(() => {
+         setupAPI(stationFilter).then(data => setStations(data))
+   }, [stationFilter])
+   
+   const setupAPI = async (stationFilter) => {
+      const api = new RadioBrowserApi(fetch.bind(window), 'UKR Radio App');
+
+      const stations = await api.searchStations({
+         countryCode: 'UA',
+         tag: stationFilter,
+         limit: 30,
+      })
+
+      return stations;
+   }
+
+   const SetDefaultSrc = (event) => {
+      event.target.src = defaultImg;
+   }
+   
+   const filters = [
+      'classical',
+      'country',
+      'dance',
+      'disco',
+      'house',
+      'jazz',
+      'pop music',
+      'retro',
+      'rock',
+      'rap',
+      'eclectic',
+      'atmospheric',
+      'alternative',
+      'folk',
+      'music'
+   ]
+   
+  return (
+   <div className="radio">
+      <div className="filters">
+      <span className={stationFilter === '' ? 'filter selected' : 'filter'}
+            onClick={() => setStationFilter('')}
+      >all</span>
+         {filters.map(filter => {
+            return (<span className={stationFilter === filter ? 'filter selected' : 'filter'} 
+            onClick={() => setStationFilter(filter)}
+            key={filter}>{filter}
+            </span>)
+         })}
+      </div>
+      <div className="stations">
+         {stations && stations.map((station, index) => {
+            console.log(station);
+            return (
+               <div className="station" key={index}>
+                  <div className="station-name">
+                     <img className="logo" 
+                        src={station.favicon} 
+                        alt={station.name} 
+                        onError={SetDefaultSrc} 
+                     />
+                     <div className="name">{station.name}</div>
+                  </div>
+                  <AudioPlayer className="player" 
+                     src={station.urlResolved}
+                     showJumpControls={false}
+                     layout='horizontal-reverse'
+                     volume={0.2}
+                     showFilledVolume={true}
+                     autoPlayAfterSrcChange={false}
+                     customProgressBarSection={[]}
+                     customControlsSection={['MAIN_CONTROLS','VOLUME_CONTROLS']}
+                  />
+               </div>
+            )
+         })}
+      </div>
+   </div>
+   )
+};
