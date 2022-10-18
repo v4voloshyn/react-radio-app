0a88c132bb44bcde00b1e9193c8ec36330adb963
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-18 18:06:36 +0300

    Chore: Organize imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 import { Radio } from './Radio';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Радіо плеєр українських радіо станцій</h1>
+      <h1>Радіо плеєр українських радіостанцій</h1>
       <h4>Виберіть жанр, виберіть станцію, насолоджуйтесь музикою</h4>
       <Radio/>
     </div>
@@ -12,4 +12,3 @@ function App() {
 }
 
 export default App;
-//http://all.api.radio-browser.info/json/stations/bycountry/Ukraine
