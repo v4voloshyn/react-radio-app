a05959fe2da4e8f2341dc9732d3830ba76082dfa
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-18 18:11:28 +0300

    Fix bug with stations fetching
    Fetch all filters for use at the next app release

--- src/Radio.jsx
+++ src/Radio.jsx
@@ -1,88 +1,95 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from 'react';
 
-import { RadioBrowserApi } from "radio-browser-api";
-import AudioPlayer from 'react-h5-audio-player'
-import 'react-h5-audio-player/lib/styles.css';
+import { RadioBrowserApi } from 'radio-browser-api';
+import AudioPlayer from 'react-h5-audio-player';
+import { FILTERS } from './constants/filters';
 import defaultImg from './assets/img/radio.svg';
+import 'react-h5-audio-player/lib/styles.css';
 
 export const Radio = () => {
+	const [stations, setStations] = useState(null);
+	const [stationFilter, setStationFilter] = useState('');
+	const [filters, setFilters] = useState([]);
 
-   const [stations, setStations] = useState();
-   const [stationFilter, setStationFilter] = useState('');
-   
-   useEffect(() => {
-         setupAPI(stationFilter).then(data => setStations(data))
-   }, [stationFilter])
-   
-   const setupAPI = async (stationFilter) => {
-      const api = new RadioBrowserApi(fetch.bind(window), 'UKR Radio App');
+	const api = new RadioBrowserApi('UKR Radio App');
 
-      const stations = await api.searchStations({
-         countryCode: 'UA',
-         tag: stationFilter,
-         limit: 30,
-      })
+	const setupAPI = useCallback(async (stationFilter) => {
+		return api.searchStations({
+			countryCode: 'UA',
+			tag: stationFilter,
+			limit: 30,
+		});
+		// eslint-disable-next-line
+	}, []);
 
-      return stations;
-   }
+	const fetchFilters = async () => {
+		const radioStations = await api.searchStations({
+			countryCode: 'UA',
+		});
+		const tags = await radioStations.map((station) => station.tags);
+		return [...new Set(tags.flat())];
+	};
 
-   const SetDefaultSrc = (event) => {
-      event.target.src = defaultImg;
-   }
-   
-   const filters = [
-      'dance',
-      'house',
-      'jazz',
-      'pop music',
-      'rock',
-      'rap',
-      'eclectic',
-      'atmospheric',
-      'alternative',
-      'folk',
-      'music'
-   ]
-   
-  return (
-   <div className="radio">
-      <div className="filters">
-      <span className={stationFilter === '' ? 'filter selected' : 'filter'}
-            onClick={() => setStationFilter('')}
-      >all</span>
-         {filters.map(filter => {
-            return (<span className={stationFilter === filter ? 'filter selected' : 'filter'} 
-            onClick={() => setStationFilter(filter)}
-            key={filter}>{filter}
-            </span>)
-         })}
-      </div>
-      <div className="stations">
-         {stations && stations.map((station, index) => {
-            return (
-               <div className="station" key={index}>
-                  <div className="station-name">
-                     <img className="logo" 
-                        src={station.favicon} 
-                        alt={station.name} 
-                        onError={SetDefaultSrc} 
-                     />
-                     <div className="name">{station.name}</div>
-                  </div>
-                  <AudioPlayer className="player" 
-                     src={station.urlResolved}
-                     showJumpControls={false}
-                     layout='horizontal-reverse'
-                     volume={0.2}
-                     showFilledVolume={true}
-                     autoPlayAfterSrcChange={false}
-                     customProgressBarSection={[]}
-                     customControlsSection={['MAIN_CONTROLS','VOLUME_CONTROLS']}
-                  />
-               </div>
-            )
-         })}
-      </div>
-   </div>
-   )
+	const SetDefaultSrc = (event) => {
+		event.target.src = defaultImg;
+	};
+
+	useEffect(() => {
+		setupAPI(stationFilter).then((data) => setStations(data));
+	}, [stationFilter, setupAPI]);
+
+	useEffect(() => {
+		fetchFilters().then((data) => setFilters(data));
+	}, []);
+
+	return (
+		<div className='radio'>
+			<div className='filters'>
+				<span
+					className={stationFilter === '' ? 'filter selected' : 'filter'}
+					onClick={() => setStationFilter('')}
+				>
+					all
+				</span>
+				{FILTERS.map((filter) => (
+					<span
+						className={stationFilter === filter ? 'filter selected' : 'filter'}
+						onClick={() => setStationFilter(filter)}
+						key={filter}
+					>
+						{filter}
+					</span>
+				))}
+			</div>
+			<div className='stations'>
+				{stations &&
+					stations.map((station, index) => {
+						return (
+							<div className='station' key={index}>
+								<div className='station-name'>
+									<img
+										className='logo'
+										src={station.favicon}
+										alt={station.name}
+										onError={SetDefaultSrc}
+									/>
+									<div className='name'>{station.name}</div>
+								</div>
+								<AudioPlayer
+									className='player'
+									src={station.urlResolved}
+									showJumpControls={false}
+									layout='horizontal-reverse'
+									volume={0.2}
+									showFilledVolume={true}
+									autoPlayAfterSrcChange={false}
+									customProgressBarSection={[]}
+									customControlsSection={['MAIN_CONTROLS', 'VOLUME_CONTROLS']}
+								/>
+							</div>
+						);
+					})}
+			</div>
+		</div>
+	);
 };
